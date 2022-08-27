73ab4d2e22eae171f07ccfc3fb047dc2eb97abcf
Serhey Hlushak <glushak_sergey@i.ua>
2022-08-27 11:21:24 +0300

    start project

---
+++ django_react_app/django_react_app/settings/internationalization_settings.py
@@ -0,0 +1,11 @@
+# Internationalization
+# https://docs.djangoproject.com/en/4.0/topics/i18n/
+import os
+
+from django.conf import settings
+
+LANGUAGE_CODE = 'en'
+TIME_ZONE = 'Europe/Kiev'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
